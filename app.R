173010ce98a174660aa1691956e814bd3d435a1b
Xiangdong Chen <dxdychen@gmail.com>
2020-03-07 23:03:47 -0500

    final committed

--- app.R
+++ app.R
@@ -8,42 +8,176 @@
 #
 
 library(shiny)
+library(DT)
+library(sparkline)
+library(dplyr)
+library(dygraphs)
+library(reshape2)
+library(shinydashboard)
+library(leaflet)
+library(htmltools)
+source("./data.R")
+global_data <- get_init_data()
+last_date = global_data[["lastdate"]]
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+ui <- dashboardPage(
+    dashboardHeader(title = "Dashboard for Coronavirus",  titleWidth = 300),
+    dashboardSidebar(disable = TRUE),
+    
+    body=  dashboardBody(
+        tags$div(
+            tags$a(href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series", 
+                   "Live Data from Johns Hopkins - Whiting School of Engineering: https://raw.githubusercontent.com/CSSEGISandData")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        fluidRow(
+            column(width=6,
+                tags$div(tags$h2(paste("last updated:", as.character(last_date))))),
+            column(width=6,
+                selectInput("selcountry", "Top 30 Countries:",
+                        c("World" = "World")))
+            
+        ),
+        fluidRow(
+            valueBoxOutput("vconfirmed"),
+            valueBoxOutput("vdeath"),
+            valueBoxOutput("vrecovered")
+        ),
+        fluidRow(
+            box(
+                width = 12, status = "info", solidHeader = TRUE,
+                title = "Time Series",
+                
+                box(width = 4,dygraphOutput("dygraphC", height='150px')),
+                box(width = 4,dygraphOutput("dygraphD", height='150px')),
+                box(width = 4,dygraphOutput("dygraphR", height='150px'))
+               
+            )
+            
+            ),
+        fluidRow(
+            box(
+                width = 12, status = "info",
+               
+                leafletOutput("mymap", width = "100%", height = 300)
+            )
         )
-    )
+   )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+            
+server <- function(input, output, session) {
+    
+    topCountries = get_top_country(global_data$confirm)
+    updateSelectInput(session, "selcountry",
+                      
+                      choices = c("World", topCountries)
+    )
+    
+    myCountry <- reactive({
+        
+        myselCountry     <- input$selcountry
+        v = strsplit(myselCountry, split=" - ")[[1]][1]
+        return (v)
+    })
+    
+    output$vconfirmed <- renderValueBox({
+        country = myCountry()
+      
+        value = get_last_value(global_data$confirm, country, last_date)
+        valueBox(
+            value = format(value, big.mark=","),  #formatC(value, digits = 1, format = "f"),
+            subtitle = "Confirmed",
+            icon = icon("users"), color="blue"
+        )
+    })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$vdeath <- renderValueBox({
+        country = myCountry()
+        value = get_last_value(global_data$death, country, last_date)
+        valueBox(
+            value = format(value, big.mark=","),
+            subtitle = "Total deaths",
+            icon = icon("users"), color="yellow"
+        )
+    })
+
+    output$vrecovered <- renderValueBox({
+        country = myCountry()
+        value = get_last_value(global_data$recovered,country, last_date)
+        valueBox(
+            value = format(value, big.mark=","),
+            "Total Recovered",
+            icon = icon("users"),  color="green"
+        )
+    })
+    
+    output$dygraphC <-   renderDygraph({
+        country = myCountry()
+        data = get_time_series(data0 = global_data$confirm, country= country)
+       
+        z = data.frame(confirm = data$new_value)
+        rownames(z) <- data$date
+        dygraph(z, main="Confirmed", group="A") %>%
+            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
+            dyHighlight(highlightCircleSize = 5, 
+                        highlightSeriesBackgroundAlpha = 0.5,
+                        hideOnMouseOut = TRUE) %>%
+            dyAxis("x", drawGrid = FALSE) %>%
+            dyAxis("y", label = "Count")  
+        
+        
+    })
+    
+    output$dygraphD <-   renderDygraph({
+        country = myCountry()
+        data = get_time_series(data0 = global_data$death, country= country)
+        z = data.frame(death = data$new_value)
+        rownames(z) <- data$date
+        dygraph(z, main="Death", group="A") %>%
+            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
+            dyHighlight(highlightCircleSize = 5, 
+                        highlightSeriesBackgroundAlpha = 0.5,
+                        hideOnMouseOut = TRUE) %>%
+            dyAxis("x", drawGrid = FALSE) %>%
+            dyAxis("y", label = "Count")  
+        
+      
+        
+        
+    })
+    output$dygraphR <-   renderDygraph({
+        country = myCountry()
+        data = get_time_series(data0 = global_data$recovered, country= country)
+        z = data.frame(recovered = data$new_value)
+        rownames(z) <- data$date
+        dygraph(z, main="Recovered", group="A") %>%
+            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
+            dyHighlight(highlightCircleSize = 5, 
+                        highlightSeriesBackgroundAlpha = 0.5,
+                        hideOnMouseOut = TRUE) %>%
+            dyAxis("x", drawGrid = FALSE) %>%
+            dyAxis("y", label = "Count")  
+        
+        
+        
+        
+    })
+   
+    output$mymap <- renderLeaflet({
+        country = myCountry()
+        data <- get_map_data(country=country, data0=global_data$confirm, date0 = last_date)
+       
+        leaflet(data) %>% addCircles(lng = ~Long, lat = ~Lat) %>% 
+            addTiles() %>% addCircleMarkers(data = data, lng = ~Long, lat = ~Lat, 
+                                            radius = 3, popup = ~as.character(content), 
+                                          
+                                            stroke = FALSE, fillOpacity = 0.8)
     })
 }
 
+
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server) 
---
+++ data.R
@@ -0,0 +1,108 @@
+library(httr)
+library(tidyr)
+library(reshape2)
+library(dplyr)
+
+
+get_init_data <- function()
+{
+   hitbub_root <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
+   github_pathConfirm <- file.path(hitbub_root, "time_series_19-covid-Confirmed.csv")
+   github_pathDeaths  <- file.path(hitbub_root, "time_series_19-covid-Deaths.csv")
+   github_pathRecovered  <- file.path(hitbub_root, "time_series_19-covid-Recovered.csv")
+   
+   c <- read.csv(github_path, header=T, stringsAsFactors = F)
+   d <- read.csv(github_pathDeaths, header=T, stringsAsFactors = F)
+   r <- read.csv(github_pathRecovered, header=T, stringsAsFactors = F)
+   
+   get_convert_wide_to_long <- function(df)
+   {
+     # Convert data from wide to long format
+     #New Format will be 
+     #    Province.State, Country.Region, Lat, Long, Date, Value
+     data_long <- melt(df, id.vars=c("Province.State", "Country.Region", "Lat", "Long"))
+     data_long$date0 <- substr(as.character(data_long$variable), 2, 8)
+     data_long$date <- as.Date(data_long$date0, '%m.%d.%y')
+     data_long$variable <- NULL
+     names(data_long)[names(data_long)=="Province.State"] <- "region"
+     names(data_long)[names(data_long)=="Country.Region"] <- "country"
+     
+     return (data_long[!is.na(data_long$value) , ])
+   }
+   c = get_convert_wide_to_long(c)
+   d = get_convert_wide_to_long(d)
+   r = get_convert_wide_to_long(r)
+   lastDate = max(c$date)
+
+   
+   return (list(lastdate= lastDate, confirm = c,death = d, recovered = r))
+   
+}
+
+
+get_last_value <- function(data0, country, date0)
+{
+   lastdata = data0[data0$date==date0, ]
+   if (country != "World")
+   {
+      lastdata = lastdata[lastdata$country==country, ]
+   }
+   r = lastdata  %>% summarise(total = sum(value))
+   
+   return ( r[1,1])
+   
+}
+
+get_map_data <- function(country,data0, date0)
+{
+   lastdata = data0[data0$date==date0, ]
+   if (country != "World")
+   {
+      lastdata = lastdata[lastdata$country==country, ]
+   }
+   
+   lastdata <- mutate(lastdata, content=paste0('<strong>confirmed: </strong>',value,
+                                          '<br><strong>Country:</strong> ', country,
+                                          '<br><strong>region:</strong> ',region)) 
+   
+   return (lastdata)
+}
+#sep = "<br/>",
+get_time_series <- function(data0, country)
+{
+   #data0 <- CCC
+   firstDate = min(data0$date)
+   lastDate =  max(data0$date)
+   if (country != "World")
+   {
+      data0 = data0[data0$country==country, ]
+   }
+   dDDD <- data0 %>% group_by(date) %>% summarise(value = sum(value)) %>%
+      mutate(prev_value = lag(value, order_by=date)) %>%
+      mutate(new_value = value -  prev_value) %>% filter(!is.na(prev_value))
+   return (dDDD)
+}
+
+get_top_country <- function(data0)
+{
+   date0 =  max(data0$date)
+   lastdata = data0[data0$date==date0, ]
+   lastdata <- lastdata %>% group_by(country) %>% summarise(value = sum(value))
+   print(lastdata)
+   mt <- lastdata[order(lastdata$value,decreasing = T ), ]
+   return (paste(mt$country[1:30], "-",  mt$value[1:30]))
+   
+}
+#ccc = get_top_country(data)
+
+
+#head(dataC$date, 1)
+
+#get_last_value(data$confirm, data$lastdate)
+
+#data <- get_init_data()$confirm
+#A <- data$confirm
+
+#get_map_data(country="World", data0=data, date0 = lastDate)
+
+
