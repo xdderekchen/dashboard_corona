0f3105ee787f9cb14347718117dadba9e2a0faf9
Xiangdong Chen <dxdychen@gmail.com>
2020-03-11 15:30:35 -0400

    commit

--- app.R
+++ app.R
@@ -1,182 +1,159 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(DT)
-library(sparkline)
-library(dplyr)
-library(dygraphs)
 library(reshape2)
 library(shinydashboard)
-library(leaflet)
 library(htmltools)
 source("./data.R")
-global_data <- get_init_data()
-last_date = global_data[["lastdate"]]
+
+#global_data <- get_init_data()
+#last_date = global_data[["lastdate"]]
+
+global_data_daily = get_daily_data()
 
 ui <- dashboardPage(
-   
-    dashboardHeader(title = "Dashboard for Coronavirus",  titleWidth = 300),
-    dashboardSidebar(disable = TRUE),
-    
-    body=  dashboardBody(
-        tags$head(includeHTML("google-analytics.html")),
+  
+  dashboardHeader(title = "DC-MD-VA Coronavirus",  titleWidth = 300, disable = TRUE),
+  dashboardSidebar(disable = TRUE),
+  
+  body=  dashboardBody(
+    tags$head(includeHTML("google-analytics.html")),
+    #tags$div(
+    #tags$h3("Most updated information pulled from State Govement's CDC sites, you may click the icon to go the CDC websites")),
+    fluidRow(
+      box(
+        width = 12, status = "info", title="Live Data from CDC sites", solidHeader = TRUE,
         
         fluidRow(
-            column(width=6,
-                tags$div(tags$h2(paste("last updated:", as.character(last_date))))),
-            column(width=6,
-                selectInput("selcountry", "Top 30 Countries:",
-                        c("World" = "World")))
-            
+             valueBoxOutput("vDC"),
+             valueBoxOutput("vMD"),
+             valueBoxOutput("vVA")
         ),
-        fluidRow(
-            valueBoxOutput("vconfirmed"),
-            valueBoxOutput("vdeath"),
-            valueBoxOutput("vrecovered")
-        ),
-        fluidRow(
-            box(
-                width = 12, status = "info", solidHeader = TRUE,
-                title = "Time Series",
-                
-                box(width = 4,dygraphOutput("dygraphC", height='150px')),
-                box(width = 4,dygraphOutput("dygraphD", height='150px')),
-                box(width = 4,dygraphOutput("dygraphR", height='150px'))
-               
-            )
-            
-            ),
-        fluidRow(
-            box(
-                width = 12, status = "info",
-               
-                leafletOutput("mymap", width = "100%", height = 300)
-            )
-        ),
-        tags$div(
-            tags$a(href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series", 
-                   "Live Data from Johns Hopkins - Whiting School of Engineering: https://raw.githubusercontent.com/CSSEGISandData")
-        )
-   )
+        div(DTOutput("chinaCDC"), style = "font-size: 65%; width: 65%")
+       
+      )
+    ),
+    
+    fluidRow(
+      box(
+        width = 12, status = "info", title="Data from John Hopkins, daily updated", solidHeader = TRUE,
+        div(DTOutput("world"), style = "font-size: 70%; width: 70%"),
+        tags$style(HTML("hr {border-top: 1px solid #000000;}")),
+        div(DTOutput("USA"), style = "font-size: 70%; width: 70%")
+      )
+    )
+   
+  )
 )
 
 
-            
+
 server <- function(input, output, session) {
-    
-    topCountries = get_top_country(global_data$confirm)
-    updateSelectInput(session, "selcountry",
-                      
-                      choices = c("World", topCountries)
+  
+ 
+  URLs = get_State_CDC_URL()
+  dc_data <- get_DC(URLs[URLs$REGION=="DC", "URL"])
+  md_data <- get_MD(URLs[URLs$REGION=="MD", "URL"])
+  va_data <- get_VA(URLs[URLs$REGION=="VA", "URL"])
+ 
+  china_data <- get_China()
+  
+  china_data <- spread(china_data, category, value)
+  rownames(china_data) <- c("CHINA CDC")
+  china_data[, c("Date", "Confirmed cases", "Deaths", "Recoveries", "Suspected cases")] -> china_data
+  
+  output$chinaCDC <- renderDT(
+    datatable(
+     data= china_data, class = "compact",
+     options = list(
+       dom = 't',
+       ordering = FALSE,
+       paging = FALSE,
+       searching = FALSE)
+   )
+  )
+  
+  
+  world_data <- get_daily_data_world(global_data_daily[[2]])
+  USA_data   <- get_daily_data_US(global_data_daily[[2]])
+  
+  
+ # output$china <- renderTable(china_data)
+  
+  output$world <- renderDT(datatable(data = world_data,class = "compact",
+                          
+                           caption = tags$caption("World", style = "color:Blue"),
+                           options = list(
+                             dom = 't',
+                             ordering = FALSE,
+                             paging = FALSE,
+                             searching = FALSE))
+                           )
+  
+  output$USA <- renderDT(datatable(data = USA_data,class = "compact",
+                        
+                         caption = tags$caption("USA", style = "color:Blue"),
+                         options = list(
+                           dom = 't',
+                           ordering = FALSE,
+                           paging = FALSE,
+                           searching = FALSE))
+  )
+  
+  #output$DC <- renderTable(dc_data[[2]])
+  #output$MD <- renderTable(md_data[[2]])
+  #output$VA <- renderTable(va_data[[2]])
+
+ 
+  
+  output$vDC <- renderValueBox({
+    if (is.na(dc_data)) {
+      value = "NA"
+    } else
+    {
+      value = format(dc_data[[1]], big.mark=",")
+    }
+    valueBox(
+      value = value,
+      "Washington DC",
+      icon = icon("users"),  color="blue",
+      href = URLs[URLs$REGION=="DC", "URL"]
     )
+  })
+  
+  output$vMD <- renderValueBox({
+    if (is.na(md_data)) {
+      value = "NA"
+    } else
+    {
+      value = format(md_data[[1]], big.mark=",")
+    }
+    valueBox(
+      value = value,
+      "Maryland",
+      icon = icon("users"),  color="blue",
+      href = URLs[URLs$REGION=="MD", "URL"]
+    )
+  })
+  
+  output$vVA <- renderValueBox({
+    if (is.na(va_data)) {
+      value = "NA"
+    } else
+    {
+      value = format(va_data[[1]], big.mark=",")
+    }
+    valueBox(
+      value = value,
+      "Virginia",
+      icon = icon("users"),  color="blue",
+      href =  URLs[URLs$REGION=="VA", "URL"]
+    )
+  })
+  
+  
     
-    myCountry <- reactive({
-        
-        myselCountry     <- input$selcountry
-        v = strsplit(myselCountry, split=" - ")[[1]][1]
-        return (v)
-    })
-    
-    output$vconfirmed <- renderValueBox({
-        country = myCountry()
-      
-        value = get_last_value(global_data$confirm, country, last_date)
-        valueBox(
-            value = format(value, big.mark=","),  #formatC(value, digits = 1, format = "f"),
-            subtitle = "Confirmed",
-            icon = icon("users"), color="blue"
-        )
-    })
-
-    output$vdeath <- renderValueBox({
-        country = myCountry()
-        value = get_last_value(global_data$death, country, last_date)
-        valueBox(
-            value = format(value, big.mark=","),
-            subtitle = "Total deaths",
-            icon = icon("users"), color="yellow"
-        )
-    })
-
-    output$vrecovered <- renderValueBox({
-        country = myCountry()
-        value = get_last_value(global_data$recovered,country, last_date)
-        valueBox(
-            value = format(value, big.mark=","),
-            "Total Recovered",
-            icon = icon("users"),  color="green"
-        )
-    })
-    
-    output$dygraphC <-   renderDygraph({
-        country = myCountry()
-        data = get_time_series(data0 = global_data$confirm, country= country)
-       
-        z = data.frame(confirm = data$new_value)
-        rownames(z) <- data$date
-        dygraph(z, main="Confirmed", group="A") %>%
-            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
-            dyHighlight(highlightCircleSize = 5, 
-                        highlightSeriesBackgroundAlpha = 0.5,
-                        hideOnMouseOut = TRUE) %>%
-            dyAxis("x", drawGrid = FALSE) %>%
-            dyAxis("y", label = "Count")  
-        
-        
-    })
-    
-    output$dygraphD <-   renderDygraph({
-        country = myCountry()
-        data = get_time_series(data0 = global_data$death, country= country)
-        z = data.frame(death = data$new_value)
-        rownames(z) <- data$date
-        dygraph(z, main="Death", group="A") %>%
-            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
-            dyHighlight(highlightCircleSize = 5, 
-                        highlightSeriesBackgroundAlpha = 0.5,
-                        hideOnMouseOut = TRUE) %>%
-            dyAxis("x", drawGrid = FALSE) %>%
-            dyAxis("y", label = "Count")  
-        
-      
-        
-        
-    })
-    output$dygraphR <-   renderDygraph({
-        country = myCountry()
-        data = get_time_series(data0 = global_data$recovered, country= country)
-        z = data.frame(recovered = data$new_value)
-        rownames(z) <- data$date
-        dygraph(z, main="Recovered", group="A") %>%
-            dyLegend(show = "onmouseover", hideOnMouseOut = FALSE) %>%
-            dyHighlight(highlightCircleSize = 5, 
-                        highlightSeriesBackgroundAlpha = 0.5,
-                        hideOnMouseOut = TRUE) %>%
-            dyAxis("x", drawGrid = FALSE) %>%
-            dyAxis("y", label = "Count")  
-        
-        
-        
-        
-    })
-   
-    output$mymap <- renderLeaflet({
-        country = myCountry()
-        data <- get_map_data(country=country, data0=global_data$confirm, date0 = last_date)
-       
-        leaflet(data) %>% addCircles(lng = ~Long, lat = ~Lat) %>% 
-            addTiles() %>% addCircleMarkers(data = data, lng = ~Long, lat = ~Lat, 
-                                            radius = 3, popup = ~as.character(content), 
-                                          
-                                            stroke = FALSE, fillOpacity = 0.8)
-    })
+ 
 }
 
 
--- data.R
+++ data.R
@@ -2,6 +2,103 @@ library(httr)
 library(tidyr)
 library(reshape2)
 library(dplyr)
+   
+   
+get_daily_data <- function()
+{
+   today = Sys.Date()
+   yesterday = today - 1
+   todayStr <- format(today, "%m-%d-%Y")
+   yesterdayStr <- format(yesterday, "%m-%d-%Y")
+   hitbub_root = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports"
+   todayPath <- file.path(hitbub_root, paste0(todayStr, ".csv"))
+   yesPath   <- file.path(hitbub_root, paste0(yesterdayStr, ".csv")  )
+   thedate = today
+   a <- NA
+   
+   if (Sys.time()> strptime("10:00 PM", "%I:%M %p") ) {   #OK, now today after 10pm, try to get today's update
+      a <- tryCatch(
+        {
+           read.csv(todayPath, header=T, stringsAsFactors = F)
+        },
+        error=function(cond) {
+           return (NA)
+        },
+        warning=function(cond) {
+           return (NA)
+        }
+     )
+   }
+   
+   if (is.na(a))
+   {
+      a <- tryCatch(
+         {
+            read.csv(yesPath, header=T, stringsAsFactors = F)
+         },
+         error=function(cond) {
+            return (NA)
+         },
+         warning=function(cond) {
+            return (NA)
+         }
+      )
+      thedate = yesterday
+   }
+   return (list(date=thedate, data=a))                      
+}
+
+
+
+get_daily_data_US <- function(all_daily_data)
+{
+   all_daily_data %>% filter(Country.Region == "US") -> USData
+   
+   USDataTotle <- USData %>% summarise(Confirmed = sum(Confirmed), 
+                                       Deaths= sum(Deaths), 
+                                       Recovered= sum(Recovered), 
+                                       Updated = max(Last.Update)) %>% mutate(State = "ALL") %>% 
+                               select(State,  Confirmed, Deaths, Recovered, Updated)
+   bind_rows(USDataTotle,
+             USData %>% select(State=Province.State,Confirmed, Deaths, Recovered, Updated = Last.Update)
+   ) -> USData
+   rownames(USData) <- USData$State
+   USData$State <- NULL
+   USData$Updated = sapply(USData$Updated,  function(x) paste(format(as.Date(x), "%m-%d"), strsplit(x, split="T")[[1]][2]))
+   return (USData)
+}
+#  get_daily_data_US(get_daily_data()[[2]]) ->A
+
+
+get_daily_data_world <- function(all_daily_data)
+{
+   world <- all_daily_data %>% summarise(Confirmed = sum(Confirmed), 
+                                Deaths= sum(Deaths), 
+                                Recovered= sum(Recovered), 
+                                Updated = max(Last.Update)) %>% 
+                  mutate(Region = "World") %>% select(Region,  Confirmed, Deaths, Recovered, Updated)
+  
+   all_daily_data %>% group_by(Country.Region) %>% summarise(Confirmed = sum(Confirmed), 
+                                                          Deaths= sum(Deaths), 
+                                                          Recovered= sum(Recovered), 
+                                                          Updated = max(Last.Update)) %>% 
+                                                select(Region= Country.Region,  Confirmed, Deaths, Recovered, Updated) -> aggregated
+   #print(lastdata)
+   mt <- aggregated[order(aggregated$Confirmed,decreasing = T ), ]
+   china <- mt %>% filter(Region == "Mainland China")
+   canada <- mt %>% filter(Region == "Canada")
+   top5 <- mt %>% filter(!(Region %in% c("Canada", "Mainland China", "US")))
+   top5 <- top5[1:5,]
+   world = bind_rows(world, china, canada, top5)
+   
+   rownames(world) <- world$Region
+   world$Region <- NULL
+   world$Updated = sapply(world$Updated,  function(x) paste(format(as.Date(x), "%m-%d"), strsplit(x, split="T")[[1]][2]))
+   return (world)
+   
+}
+
+#  get_daily_data_world(get_daily_data()[[2]])
 
 
 get_init_data <- function()
@@ -11,7 +108,7 @@ get_init_data <- function()
    github_pathDeaths  <- file.path(hitbub_root, "time_series_19-covid-Deaths.csv")
    github_pathRecovered  <- file.path(hitbub_root, "time_series_19-covid-Recovered.csv")
    
-   c <- read.csv(github_path, header=T, stringsAsFactors = F)
+   c <- read.csv(github_pathConfirm, header=T, stringsAsFactors = F)
    d <- read.csv(github_pathDeaths, header=T, stringsAsFactors = F)
    r <- read.csv(github_pathRecovered, header=T, stringsAsFactors = F)
    
@@ -38,7 +135,7 @@ get_init_data <- function()
    return (list(lastdate= lastDate, confirm = c,death = d, recovered = r))
    
 }
-
+get_init_data()[[2]] -> AAA
 
 get_last_value <- function(data0, country, date0)
 {
@@ -93,6 +190,232 @@ get_top_country <- function(data0)
    return (paste(mt$country[1:30], "-",  mt$value[1:30]))
    
 }
+library(dplyr)
+library(rvest)
+library(gdata)
+
+get_State_CDC_URL <- function()
+{
+   str <- "REGION,URL
+DC,https://coronavirus.dc.gov/page/coronavirus-data
+MD,https://phpa.health.maryland.gov/Pages/Novel-coronavirus.aspx
+VA,http://www.vdh.virginia.gov/surveillance-and-investigation/novel-coronavirus/
+CHINA,http://weekly.chinacdc.cn/news/TrackingtheEpidemic.htm"
+   return (read.csv(text=str, stringsAsFactors = F))
+}
+
+get_DC <- function(DC_URL)
+{
+  
+  tryCatch(
+  {
+   raw_data <- xml2::read_html(DC_URL)
+   data <- raw_data %>% html_nodes("p")
+   print(xml_text(data[2]))
+   updated_DateTime <- xml_text(data)
+   
+   contents <- raw_data %>% html_nodes("ul") %>%  html_nodes("li") %>% html_text(trim = TRUE)
+   line = NA
+   for (i in seq_along(contents))
+   {
+      if (gdata::startsWith(contents[i], "Number of")) {
+         if (is.na(line))
+         {
+            line = contents[i]
+         }
+         else
+         {
+            line = paste(line, "\n", contents[i])
+         }
+         newline = contents[i]
+         if (grepl('positive|Positive', newline))
+         {
+            positiveCount = trim( strsplit(newline, split=":")[[1]][2])
+            #print(positiveCount)
+         }
+      }
+   }
+   #return (read.csv(text=line, header=F, sep= ":", col.names = c("category", "count"), stringsAsFactors=FALSE))
+   return (list(pvalue=positiveCount, 
+                rawdata = read.csv(text=line, header=F, sep= ":", 
+                                   col.names = c("category", "value"), 
+                                   colClasses = c("character",  "integer"), 
+                                   stringsAsFactors=FALSE)))
+  },
+  error=function(cond) {
+     return (NA)
+  },
+  warning=function(cond) {
+     return (NA)
+  })
+}
+#   get_DC()
+
+#################
+
+get_MD <- function(MD_URL)
+{
+   tryCatch(
+      {
+   #MD_URL = "https://phpa.health.maryland.gov/Pages/Novel-coronavirus.aspx"
+   raw_data <- xml2::read_html(MD_URL)
+   
+   contents <- raw_data %>% html_nodes("div") 
+
+   line = NA
+   positiveCount = 0
+   for (i in seq_along(contents))
+   {
+      if (gdata::startsWith(html_text(contents[i]), "Number of")) {
+         if (is.na(line))
+         {
+            line = html_text(contents[i], trim = TRUE)
+         }
+         else
+         {
+            line = paste(line, "\n", html_text(contents[i], trim = TRUE))
+         }
+         newline = html_text(contents[i], trim = TRUE)
+         if (grepl('positive|Positive', newline))
+         {
+            positiveCount = trim( strsplit(newline, split=":")[[1]][2])
+            #print(positiveCount)
+         }
+      }
+   }
+   return (list(pvalue=positiveCount, 
+                rawdata = read.csv(text=line, header=F, sep= ":", 
+                                   col.names = c("category", "value"), 
+                                   colClasses = c("character",  "integer"), 
+                                   stringsAsFactors=FALSE)))
+      },
+   error=function(cond) {
+      return (NA)
+   },
+   warning=function(cond) {
+      return (NA)
+   })
+  # return (read.csv(text=line, header=F, sep= ":", col.names = c("category", "value"), stringsAsFactors=FALSE))
+}
+#   get_MD("https://phpa.health.maryland.gov/Pages/Novel-coronavirus.aspx")
+
+
+get_VA <- function(VA_URL)
+{
+   tryCatch(
+      {
+   #VA_URL = "http://www.vdh.virginia.gov/surveillance-and-investigation/novel-coronavirus/"
+   #VA_URL="https://vdhdata.vdh.virginia.gov/views/COVID-19inVirginia/External?:embed=y&amp;:isGuestRedirectFromVizportal=y&amp;:display_count=no&amp;:showVizHome=no&amp;:toolbar=yes"
+   raw_data <- xml2::read_html(VA_URL)
+   contents <- raw_data %>% html_nodes("p") 
+   line = NA
+   positiveCount = 0
+   for (i in seq_along(contents))
+   {
+      if (gdata::startsWith(html_text(contents[i]), "Number of")) {
+         if (is.na(line))
+         {
+            line = html_text(contents[i], trim = TRUE)
+         }
+         else
+         {
+            line = paste(line, "\n", html_text(contents[i], trim = TRUE))
+         }
+         newline = html_text(contents[i], trim = TRUE)
+         if (grepl('positive|Positive', newline))
+         {
+            positiveCount = trim( strsplit(newline, split=":")[[1]][2])
+            #print(positiveCount)
+         }
+
+      }
+   }
+   return (list(pvalue=positiveCount, 
+                rawdata = read.csv(text=line, header=F, sep= ":", 
+                                   col.names = c("category", "value"), 
+                                   colClasses = c("character",  "integer"), 
+                                   stringsAsFactors=FALSE)))
+},
+error=function(cond) {
+   return (NA)
+},
+warning=function(cond) {
+   return (NA)
+})
+}
+#VA_URL = "http://www.vdh.virginia.gov/surveillance-and-investigation/novel-coronavirus/"
+#get_VA(VA_URL)[[2]] ->a
+
+get_China0 <- function()
+{
+   China_URL = "http://weekly.chinacdc.cn/news/TrackingtheEpidemic.htm"
+   raw_data <- xml2::read_html(China_URL)
+   
+   contents <- raw_data %>% html_nodes("p") 
+   found = F
+   line = NA
+   for (i in seq_along(contents))
+   {
+      if (gdata::startsWith(html_text(contents[i]), "National Health Commission Update"))
+      {
+         if (found == F)
+         {
+            found <- T
+            next
+         }
+         else 
+         {
+            break
+         }
+      } 
+      if (found == T) {
+         if (is.na(line))
+         {
+            line = paste0("Date :", html_text(contents[i], trim = TRUE))
+         }
+         else
+         {
+            line = paste(line, "\n", html_text(contents[i], trim = TRUE))
+         }
+      }
+   }
+   line = gsub("\\.", "\n", line)
+   
+ 
+   data = read.csv(text=line, header=F, sep= ":",  col.names = c("category", "value"), stringsAsFactors=FALSE, strip.white = T)
+   data %>% dplyr::filter(!(category==" ")) %>% dplyr::filter(!(is.na(category))) 
+}
+
+#get_China0()
+
+get_China <- function()
+{
+   china_data_cache = "china.rds"
+   if(!file.exists(china_data_cache)){
+      data = get_China0()
+      saveRDS(data, file = china_data_cache)
+      return (data)
+   } else
+   {
+      finf = file.info(china_data_cache)
+      if (difftime(Sys.time(), finf[,"mtime"], units = "hours") > 1)
+      {
+         print("redo")
+         data = get_China0()
+         saveRDS(data, file = china_data_cache)
+         return (data)
+      }
+      else 
+      {
+         print("use cache")
+         return (readRDS(china_data_cache))
+      }
+   }
+}
+
+
+#  get_China()
+
 #ccc = get_top_country(data)
 
 
@@ -104,5 +427,22 @@ get_top_country <- function(data0)
 #A <- data$confirm
 
 #get_map_data(country="World", data0=data, date0 = lastDate)
+#library(googlesheets4)
+#get_google <- function()
+#{
+#   a =read_sheet("https://docs.google.com/spreadsheets/d/1vAr_WyP6ljZljM2g8puSN23X8spJV2m2Ya5PJ5yGZyk/edit?usp=sharing", )
+
+#   return (a)
+#}
+
+
+#get_VAgoogle <- function()
+#{
+#   a =read_sheet("https://docs.google.com/spreadsheets/d/1vAr_WyP6ljZljM2g8puSN23X8spJV2m2Ya5PJ5yGZyk/edit?usp=sharing")
+#   a %>% filter(region == "VA") %>% select (category, value) -> b
+#   return (b)
+#}
+
+### get_VA()
 
 
